13286299bfafbe5eba87d813eafac78029f93658
kvt500 <kvt500@gmail.com>
2023-01-03 11:58:04 +0500

    x

--- app.rb
+++ app.rb
@@ -2,7 +2,47 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db =SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash = true
+end
+
+before do
+
+	init_db
+
+end
+
+configure do
+	init_db
+	@db.execute 'create table if not exists Posts
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_date DATE,
+		content TEXT
+	)'
+end
+
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/new' do
+	content = params[:content]
+
+	if content.length <= 0
+		@error = 'Type post text'
+		return erb :new
+	end
+
+	@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
+
+	erb "You typed #{content}"
 end
